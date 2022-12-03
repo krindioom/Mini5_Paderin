492799d78073d26e1b31f17d148d7ec70470479f
krindioom <yoerfrost1234@gmail.com>
2022-12-04 02:20:40 +0700

    my_pow_func_v1

---
+++ main1.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+
+
+double my_pow(double indicator, unsigned int degree)    
+{
+	double finalNumber = 1;
+	for (int i = 0; i < degree; i++)
+	{
+		finalNumber *= indicator;
+	}
+
+	return finalNumber;
+}
+
+int main()
+{
+	int degree;
+	double indicator;
+
+	cin >> indicator >> degree;
+	double number = my_pow(indicator, degree);
+
+	cout << number;
+
+	return 0;
+}
